2f8d3114a271ffa4b88d6fed33edb260a998ed16
Jake W <jake@playruyi.com>
2018-04-18 14:52:55 +0800

    Merge branch 'development' of http://bitbucket:7990/scm/ruyi/jade into feature/mobile_app

---
+++ ThriftFiles/StorageLayer/StorageLayerSDKServices.thrift
@@ -0,0 +1,9 @@
+include "StorageLayerSDKDataTypes.thrift"
+
+namespace csharp Ruyi.SDK.StorageLayer
+namespace cpp Ruyi.SDK.StorageLayer
+
+service StorageLayerService {
+	StorageLayerSDKDataTypes.GetLocalPathResult GetLocalPath(1: string msg),
+}
+
