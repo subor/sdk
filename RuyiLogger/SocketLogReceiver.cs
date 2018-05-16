d0c5d1d114bd8634965623d3c89396d6768d00c4
Jake W <jake@playruyi.com>
2018-05-16 12:22:45 +0800

    [Power] Fix cancel operation

---
+++ RuyiLogger/SocketLogReceiver.cs
@@ -0,0 +1,67 @@
+﻿using RuyiLogger;
+using NetMQ;
+using NetMQ.Sockets;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ruyi.DevTool
+{
+    public class SocketLogReceiver
+    {
+        public delegate void ReceiveCallback(LoggerMessage msg);
+
+        CancellationTokenSource cancel = new CancellationTokenSource();
+        DealerSocket subSocket = new DealerSocket();
+        ReceiveCallback receiveCallback = null;
+
+        public void Connect(string url, string identity, ReceiveCallback cb)
+        {
+            subSocket.Options.Identity = Encoding.ASCII.GetBytes(identity);
+            subSocket.Connect(url);
+            subSocket.ReceiveReady += Receive;
+            receiveCallback = cb;
+
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    subSocket.Poll();
+                }
+            }, cancel.Token);
+        }
+
+        public void Stop()
+        {
+            // TODO, send stop msg to broker to stop the socket logger.
+            cancel.Cancel();
+            subSocket.Close();
+            subSocket.Dispose();
+        }
+
+        void Receive(object sender, NetMQSocketEventArgs evt)
+        {
+            NetMQMessage msg = null;
+            try
+            {
+                msg = subSocket.ReceiveMultipartMessage();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("pub-sub client receive exception: " + e.Message);
+            }
+            var msgType = msg.Last.ConvertToString();
+            LoggerMessage lm = JsonConvert.DeserializeObject<LoggerMessage>(msgType);
+            receiveCallback?.Invoke(lm);
+        }
+
+        public void PlaybackLog(NetMQMessage msg)
+        {
+            msg.PushEmptyFrame();
+            subSocket.SendMultipartMessage(msg);
+        }
+
+    }
+}
