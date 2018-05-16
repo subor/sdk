bc37e7695893d2d65aef09731f89ca2a7c6d7373
jake <jake@playruyi.com>
2018-05-16 22:53:35 +0800

    [Layer0] Split ServiceManager in two

---
+++ UnitTests/Elo/FideEloCalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Ruyi.SDK.Online.Tests.Elo
+{
+    [TestFixture]
+    public class FideEloCalculatorTest
+    {
+        [Test]
+        public void FideProvisionalEloCalculatorTests()
+        {
+            var calc = new FideEloCalculator(new FideKFactor.Provisional());
+
+            EloAssert.AssertChessRating(calc, 1200, 1500, PairwiseComparison.Win, 1221.25, 1478.75);
+            EloAssert.AssertChessRating(calc, 1200, 1500, PairwiseComparison.Draw, 1208.75, 1491.25);
+            EloAssert.AssertChessRating(calc, 1200, 1500, PairwiseComparison.Lose, 1196.25, 1503.75);
+        }
+
+        [Test]
+        public void FideNonProvisionalEloCalculatorTests()
+        {
+            var calc = new FideEloCalculator();
+
+            EloAssert.AssertChessRating(calc, 1200, 1200, PairwiseComparison.Win, 1207.5, 1192.5);
+            EloAssert.AssertChessRating(calc, 1200, 1200, PairwiseComparison.Draw, 1200, 1200);
+            EloAssert.AssertChessRating(calc, 1200, 1200, PairwiseComparison.Lose, 1192.5, 1207.5);
+
+            EloAssert.AssertChessRating(calc, 2600, 2500, PairwiseComparison.Win, 2603.6, 2496.4);
+            EloAssert.AssertChessRating(calc, 2600, 2500, PairwiseComparison.Draw, 2598.6, 2501.4);
+            EloAssert.AssertChessRating(calc, 2600, 2500, PairwiseComparison.Lose, 2593.6, 2506.4);
+        }
+    }
+}
