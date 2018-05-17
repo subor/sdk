9447d1ebc3a2244ab7bd2e9d7a7ae6d0838bcb1e
jake <jake@playruyi.com>
2018-05-17 23:48:17 +0800

    [Devtool] Fix a bunch of tests

---
+++ RuyiCppSDKTest/TestCase/BCServiceTest.h
@@ -0,0 +1,143 @@
+#pragma once
+
+#include "Layer0Test.h"
+#include <windows.h>
+#include <process.h>
+
+#include "json.h"
+#include "BaseUnitTest.h"
+#include "RuyiNet/RuyiNetClient.h"
+
+using namespace Ruyi;
+using namespace Ruyi::SDK;
+using namespace boost::property_tree;
+using namespace std;
+using namespace Ruyi::SDK::BrainCloudApi;
+
+class BCServiceTest : public BaseUnitTest
+{
+public:
+	BCServiceTest(RuyiSDKContext::Endpoint endpoint = RuyiSDKContext::Endpoint::Console, string remoteAddress = "localhost");
+
+	void BCS_Authentication_AuthenticateAnonymous(); //order(0)
+	void BCS_Authentication_ClearSavedProfiedId(); //order(10)
+	void BCS_Authentication_AuthenticateEmail(); //order(20)
+	void BCS_Authentication_ResetEmailPassword(); //order(22)
+	void BCS_Authentication_AuthenticateUniversal(); //order(30)
+	void BCS_AsyncMatch_CreateMatch(); //order(50)
+	void BCS_AsyncMatch_FindMatchs(); //order(52)
+	void BCS_AsyncMatch_UpdateMatchSummaryData(); //order(55)
+	void BCS_AsyncMatch_ReadMatch(); //order(60)
+	void BCS_AsyncMatch_SubmitTurn(); //order(70)
+	void BCS_AsyncMatch_ReadMatchHistory(); //order(80)
+	void BCS_AsyncMatch_CompleteMatch(); //order(90)
+	void BCS_AsyncMatch_FindCompleteMatches(); //order(100)
+	void BCS_AsyncMatch_CreateMatchWithInitialTurn(); //order(110)
+	void BCS_AsyncMatch_AbandonMatch(); //order(120)
+	void BCS_AsyncMatch_DeleteMatch(); //order(130)
+	void BCS_DataStream_CustomPageEvent(); //order(140)
+	void BCS_DataStream_CustomScreenEvent(); //order(150)
+	void BCS_DataStream_CustomTrackEvent(); //order(160)
+	void BCS_Entity_CreateEntity(); //order(170)
+	void BCS_Entity_GetList(); //order(190)
+	void BCS_Entity_GetListCount(); //order(200)
+	void BCS_Entity_GetEntity(); //order(210)
+	void BCS_Entity_GetEntitiesByType(); //order(220)
+	void BCS_Entity_GetPage(); //order(230)
+	void BCS_Entity_GetPageOffset(); //order(240)
+	void BCS_Entity_GetSharedEntityForProfileId(); //order(250)
+	void BCS_Entity_GetSharedEntitiesForProfileId(); //order(260)
+	void BCS_Entity_GetSharedEntitiesListForProfileId(); //order(270)
+	void BCS_Entity_IncrementUserEntityData(); //order(280)
+	void BCS_Entity_IncrementSharedUserEntityData(); //order(290)
+	void BCS_Entity_UpdateEntity(); //order(300)
+	void BCS_Entity_UpdateSharedEntity(); //order(320)
+	void BCS_Entity_DeleteEntity(); //order(330)
+	void BCS_Entity_UpdateSingleton(); //order(340)
+	void BCS_Entity_GetSingleton(); //order(350)
+	void BCS_Entity_DeleteSingleton(); //order(360)
+
+	void BCS_Event_SendEvent(); //order(400)
+	void BCS_Event_GetEvents(); //order(410)
+	void BCS_Event_UpdateIncomingEventData(); //order(420)
+	void BCS_Event_DeleteIncomingEvent(); //order(430)
+
+	void BCS_File_UploadFile(); //order(440)
+	void BCS_File_GetCDNUrl(); //order(445)
+	void BCS_File_GetUploadProgressAndBytes(); //order(450) not finish
+	void BCS_File_CancelUpload(); //order(470)
+	void BCS_File_ListUserFiles(); //order(480)
+	void BCS_File_DeleteFiles(); //order(490)
+
+	void BCS_Friend_AddFriends(); //order(500)
+	void BCS_Friend_ListFirends(); //order(510)
+	void BCS_Friend_ReadFriendEntity(); //order(520)
+	void BCS_Friend_ReadFriendsEntities(); //order(530)
+	void BCS_Friend_ReadFriendUserState(); //order(540)
+	void BCS_Friend_FindUserByUniversalId(); //order(550)
+	void BCS_Friend_FindUserByExactName(); //order(560)
+	void BCS_Friend_FindUsersBySubstrName(); //order(570)
+	void BCS_Friend_GetExternalIdForProfileId(); //order(580)
+	void BCS_Friend_GetProfileInfoForCredential(); //order(590)
+	void BCS_Friend_GetSummaryDataForProfileId(); //order(600)
+	void BCS_Friend_GetUsersOnlineStatus(); //order(610)
+
+	void BCS_Gamification_ReadAllGamification(); //order(620)
+	void BCS_Gamification_ReadXpLevelsMetaData(); //order(630)
+	void BCS_Gamification_AwardAchievements(); //order(640)
+	void BCS_Gamification_ReadAchievements(); //order(650)
+	void BCS_Gamification_ReadAchievedAchievements(); //order(660)
+	void BCS_Gamification_ReadMilestones(); //order(670)
+	void BCS_Gamification_ReadMilestonesByCategory(); //order(680)
+	void BCS_Gamification_ReadInProgressMilestones(); //order(690)
+	void BCS_Gamification_ReadCompletedMilestones(); //order(700)
+	void BCS_Gamification_ResetMilestones(); //order(710)
+	void BCS_Gamification_ReadQuests(); //order(720)
+	void BCS_Gamification_ReadQuestsByCategory(); //order(730)
+	void BCS_Gamification_ReadQuestsWithStatus(); //order(740)
+	void BCS_Gamification_ReadQuestsWithBasicPercentage(); //order(750)
+	void BCS_Gamification_ReadQuestsWithComplexPercentage(); //order(760)
+	void BCS_Gamification_ReadNotStartedQuests(); //order(770)
+	void BCS_Gamification_ReadInProgressQuests(); //order(780)
+	void BCS_Gamification_ReadCompletedQuests(); //order(790)
+
+	void BCS_GlobalApp_ReadProperties(); //order(800)
+
+	void BCS_GlobalEntity_CreateEntity(); //order(810)
+	void BCS_GlobalEntity_CreateEntityWithIndexedId(); //order(820)
+	void BCS_GlobalEntity_DoEntityUpdates(); //order(830)
+	void BCS_GlobalEntity_Retrieval(); //order(835)
+	void BCS_GlobalEntity_SystemEntities(); //order(840)
+	void BCS_GlobalEntity_DeleteEntities(); //order(850)
+
+	void BCS_GlobalStatistics_IncrementGlobalStats(); //order(860)
+	void BCS_GlobalStatistics_ProcessStatistics(); //order(870)
+	void BCS_GlobalStatistics_ReadAllGlobalStats(); //order(880)
+	void BCS_GlobalStatistics_ReadGlobalStatsForCategory(); //order(890)
+	void BCS_GlobalStatistics_ReadGlobalStatsSubset(); //order(900)
+
+	//example
+	void BCS_GetLoginedUserProfile();
+	void BCS_PostScoreToLeaderboard();
+	void BCS_GetLeadboardPage();
+	void BCS_JsonTest();
+	void BCS_FriendRelatedScript();
+	
+private:
+	SubscribeClient* sdkSubscriber;
+
+	string LoginTestUser(string user = "", string pass = "", bool bRelogin = false);
+
+	Json::Reader JsonReader;
+	Json::Value _TestUserInfoRoot;
+
+	Json::Value _tkMatchInfo;
+	string _playerId1;
+	string _playerId2;
+
+	string _entityPageContext;
+
+	string _TestEventId;
+
+	Ruyi::RuyiNetClient* ruyiNetClient;
+};
---
+++ RuyiSDK/RuyiNet/Service/RuyiNetCloudService.cs
@@ -0,0 +1,303 @@
+﻿using Newtonsoft.Json;
+using Ruyi.SDK.StorageLayer;
+using System;
+using System.IO;
+using System.Security;
+
+namespace Ruyi.SDK.Online
+{
+    /// <summary>
+    /// Handles backing up data to the cloud.
+    /// </summary>
+    public class RuyiNetCloudService : RuyiNetService
+    {
+        const string BACKUP_LOCATION = "ruyiBackup";
+        readonly string[] IGNORE =
+        {
+            "Crashes",
+            "Unity",
+            "output_log.txt"
+        };
+
+        /// <summary>
+        /// Create the Cloud Service.
+        /// </summary>
+        /// <param name="client">The Ruyi Net client.</param>
+        /// <param name="storageLayerService">The storage layer service, needed to read/write files from disk</param>
+        internal RuyiNetCloudService(RuyiNetClient client, StorageLayerService.Client storageLayerService)
+        : base(client)
+        {
+            mStorageLayerService = storageLayerService;
+        }
+
+        /// <summary>
+        /// Manually backup the save data up to this point.
+        /// </summary>
+        /// <param name="index">The index of user</param>
+        /// <param name="callback">The function to call when the task completes.</param>
+        public void BackupData(int index, RuyiNetTask<RuyiNetResponse>.CallbackType callback)
+        {
+            BackupData(index, false, callback);
+        }
+
+        /// <summary>
+        /// Manually restore the save data up to this point.
+        /// </summary>
+        /// <param name="index">The index of user</param>
+        /// <param name="callback">The function to call when the task completes.</param>
+        public void RestoreData(int index, RuyiNetTask<RuyiNetResponse>.CallbackType callback)
+        {
+            EnqueueTask(() =>
+            {
+                try
+                {
+                    var path = GetPersistentDataPath(index);
+                    path = Path.GetFullPath(path);
+
+                    //  Backup persistent data path
+                    /*var backupPath = Path.Combine(path, BACKUP_LOCATION);
+                    if (Directory.Exists(backupPath))
+                    {
+                        var x = new DirectoryInfo(backupPath);
+                        x.Delete(true);
+                    }
+
+                    foreach (var i in Directory.GetDirectories(path, "*", SearchOption.AllDirectories))
+                    {
+                        var directoryName = Path.GetFileName(i);
+                        if (Array.IndexOf(IGNORE, directoryName) >= 0)
+                        {
+                            continue;
+                        }
+
+                        Directory.CreateDirectory(i.Replace(path, backupPath));
+                    }
+
+                    //Copy all the files & Replaces any files with the same name
+                    foreach (var i in Directory.GetFiles(path, "*.*", SearchOption.AllDirectories))
+                    {
+                        var fileName = Path.GetFileName(i);
+                        if (Array.IndexOf(IGNORE, fileName) >= 0)
+                        {
+                            continue;
+                        }
+
+                        File.Copy(i, i.Replace(path, backupPath), true);
+                        File.Delete(i);
+                    }*/
+
+                    //  List files
+                    var data = mClient.BCService.File_ListUserFiles_SNSFO(GetCloudLocation(index), true, index);
+                    var response = JsonConvert.DeserializeObject<RuyiNetListUserFilesResponse>(data, new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore
+                    });
+
+                    if (response.status != RuyiNetHttpStatus.OK)
+                    {
+                        return data;
+                    }
+
+                    //  Download one by one to correct path
+                    //  TODO: Need to get the session ID and pass it in as a URL parameter.
+                    foreach (var i in response.data.fileList)
+                    {
+                        EnqueueTask(() =>
+                        {
+                            return mClient.BCService.File_DownloadFile(i.cloudPath, i.cloudFilename, true, index);
+                        }, callback);
+                    }
+
+                    //  Delete backup
+                    //var backupInfo = new DirectoryInfo(backupPath);
+                    //backupInfo.Delete(true);
+
+                    return JsonConvert.SerializeObject(new RuyiNetResponse() { status = RuyiNetHttpStatus.OK, message = "Restore Data Successful" });
+                }
+                catch (Exception e)
+                {
+                    if (e is ArgumentException ||
+                        e is NotSupportedException ||
+                        e is PathTooLongException ||
+                        e is DirectoryNotFoundException)
+                    {
+                        var response = new RuyiNetResponse()
+                        {
+                            status = 400,
+                            message = e.ToString()
+                        };
+
+                        return JsonConvert.SerializeObject(response);
+                    }
+                    else if (e is SecurityException ||
+                             e is IOException ||
+                             e is UnauthorizedAccessException ||
+                             e is RuyiNetException)
+                    {
+                        var response = new RuyiNetResponse()
+                        {
+                            status = 500,
+                            message = e.ToString()
+                        };
+
+                        return JsonConvert.SerializeObject(response);
+                    }
+                    else
+                    {
+#if DEBUG
+                        var response = new RuyiNetResponse()
+                        {
+                            status = 999,
+                            message = e.ToString()
+                        };
+
+                        return JsonConvert.SerializeObject(response);
+#else
+                        throw;
+#endif
+                    }
+                }
+
+            }, callback);
+
+        }
+
+        internal void BackupData(int index, bool cleanMode, RuyiNetTask<RuyiNetResponse>.CallbackType callback)
+        {
+            EnqueueTask(() =>
+            {
+                try
+                {
+                    var path = GetPersistentDataPath(index);
+                    path = Path.GetFullPath(path);
+                    BackupPath(index, GetCloudLocation(index), path);
+                    if (cleanMode)
+                    {
+                        var di = new DirectoryInfo(path);
+                        foreach (var i in di.GetFiles())
+                        {
+                            if (Array.IndexOf(IGNORE, i.Name) >= 0)
+                            {
+                                continue;
+                            }
+
+                            i.Delete();
+                        }
+
+                        foreach (var i in di.GetDirectories())
+                        {
+                            if (Array.IndexOf(IGNORE, i.Name) >= 0)
+                            {
+                                continue;
+                            }
+
+                            i.Delete(true);
+                        }
+                    }
+
+                    return JsonConvert.SerializeObject(new RuyiNetResponse() { status = RuyiNetHttpStatus.OK });
+                }
+                catch (Exception e)
+                {
+                    if (e is ArgumentException ||
+                        e is NotSupportedException ||
+                        e is PathTooLongException ||
+                        e is DirectoryNotFoundException)
+                    {
+                        var response = new RuyiNetResponse()
+                        {
+                            status = 400,
+                            message = e.ToString()
+                        };
+
+                        return JsonConvert.SerializeObject(response);
+                    }
+                    else if (e is SecurityException ||
+                             e is IOException ||
+                             e is UnauthorizedAccessException ||
+                             e is RuyiNetException)
+                    {
+                        var response = new RuyiNetResponse()
+                        {
+                            status = 500,
+                            message = e.ToString()
+                        };
+
+                        return JsonConvert.SerializeObject(response);
+                    }
+                    else
+                    {
+                        var response = new RuyiNetResponse()
+                        {
+                            status = 501,
+                            message = e.ToString()
+                        };
+
+                        return JsonConvert.SerializeObject(response);
+
+                        //throw;
+                    }
+                }
+
+            }, callback);
+        }
+
+        private void BackupPath(int index, string cloudPath, string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var subdirectories = Directory.GetDirectories(path);
+            foreach (var i in subdirectories)
+            {
+                var directoryName = Path.GetFileName(i);
+                if (Array.IndexOf(IGNORE, directoryName) >= 0)
+                {
+                    continue;
+                }
+
+                var newPath = Path.Combine(cloudPath, directoryName);
+                BackupPath(index, newPath, i);
+            }
+
+            var files = Directory.GetFiles(path);
+            foreach (var i in files)
+            {
+                var fileName = Path.GetFileName(i);
+                if (Array.IndexOf(IGNORE, fileName) >= 0)
+                {
+                    continue;
+                }
+
+                //  TODO: Set 'shareable' to false and get the session ID to use
+                //  as a parameter when downloading the data.
+                var data = mClient.BCService.File_UploadFile(cloudPath, fileName, true/*false*/, true, i, index);
+                var response = JsonConvert.DeserializeObject<RuyiNetUploadFileResponse>(data);
+                if (response.status != RuyiNetHttpStatus.OK)
+                {
+                    throw new RuyiNetException("Failed to upload file: " + i);
+                }
+            }
+        }
+
+        private string GetPersistentDataPath(int index)
+        {
+            var path = mStorageLayerService.GetLocalPath("/<HTTPHDDCACHE>/" + GetCloudLocation(index));
+            if (path.Result)
+            {
+                return path.Path;
+            }
+
+            return null;
+        }
+
+        private string GetCloudLocation(int index)
+        {
+            return mClient.CurrentPlayers[index].profileId + "/" + mClient.AppId;
+        }
+
+        private StorageLayerService.Client mStorageLayerService;
+    }
+}
