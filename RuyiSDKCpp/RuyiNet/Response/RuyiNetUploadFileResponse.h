4ca183032e275764713c3b51b4fe6b6b25b938cd
PLAYRUYIchun <Huaichun.tao@playruyi.com>
2018-06-08 10:13:18 +0800

    [JP-2076] patch unit test

---
+++ RuyiSDKCpp/RuyiNet/Response/RuyiNetUploadFileResponse.h
@@ -0,0 +1,168 @@
+#pragma once
+
+#include <string>
+
+#include "../RuyiNetClient.h"
+
+namespace Ruyi
+{
+	/// <summary>
+	/// The response after uploading a file.
+	/// </summary>
+	struct RuyiNetUploadFileResponse
+	{
+		/// <summary>
+		/// The response data.
+		/// </summary>
+		struct Data
+		{
+			/// <summary>
+			/// The details of the file to upload.
+			/// </summary>
+			struct FileDetails
+			{
+				/// <summary>
+				/// When the file was last updated.
+				/// </summary>
+				long updatedAt;
+				/// <summary>
+				/// The size of the file.
+				/// </summary>
+				int fileSize;
+				/// <summary>
+				/// The file type.
+				/// </summary>
+				std::string fileType;
+				/// <summary>
+				/// When the file expires.
+				/// </summary>
+				long expiresAt;
+				/// <summary>
+				/// Whether or not the file is shareable.
+				/// </summary>
+				bool shareable;
+				/// <summary>
+				/// Whether or not the file is shareable.
+				/// </summary>
+				std::string uploadId;
+				/// <summary>
+				/// When the file was created.
+				/// </summary>
+				long createdAt;
+				/// <summary>
+				/// The profile ID of the owning player.
+				/// </summary>
+				std::string profileId;
+				/// <summary>
+				/// The ID of the game the file relates to.
+				/// </summary>
+				std::string gameId;
+				/// <summary>
+				/// The path to the file.
+				/// </summary>
+				std::string path;
+				/// <summary>
+				/// The filename.
+				/// </summary>
+				std::string filename;
+				/// <summary>
+				/// Whether the file will replace a file if it already exists.
+				/// </summary>
+				bool replaceIfExists;
+				/// <summary>
+				/// The location of the file on the cloud.
+				/// </summary>
+				std::string cloudPath;
+			};
+			
+			/// <summary>
+			/// The details of the file to upload.
+			/// </summary>
+			FileDetails fileDetails;
+		};
+		
+		/// <summary>
+		/// The response data.
+		/// </summary>
+		Data data;
+		/// <summary>
+		/// The status of the response.
+		/// </summary>
+		int status;
+
+		void parseJson(nlohmann::json& j)
+		{
+			if (!j["status"].is_null())
+			{
+				status = j["status"];
+			}
+
+			if (!j["data"].is_null())
+			{
+				nlohmann::json dataJson = j["data"];
+
+				if (!dataJson.is_object()) return;
+
+				if (!dataJson["fileDetails"].is_null())
+				{
+					nlohmann::json fileDetailsJson = dataJson["fileDetails"];
+					
+					if (!fileDetailsJson.is_object()) return;
+					
+					if (!fileDetailsJson["updatedAt"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["updatedAt"];
+					}
+					if (!fileDetailsJson["fileSize"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["fileSize"];
+					}
+					if (!fileDetailsJson["fileType"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["fileType"];
+					}
+					if (!fileDetailsJson["expiresAt"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["expiresAt"];
+					}
+					if (!fileDetailsJson["shareable"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["shareable"];
+					}
+					if (!fileDetailsJson["uploadId"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["uploadId"];
+					}
+					if (!fileDetailsJson["createdAt"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["createdAt"];
+					}
+					if (!fileDetailsJson["profileId"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["profileId"];
+					}
+					if (!fileDetailsJson["gameId"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["gameId"];
+					}
+					if (!fileDetailsJson["path"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["path"];
+					}
+					if (!fileDetailsJson["filename"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["filename"];
+					}
+					if (!fileDetailsJson["replaceIfExists"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["replaceIfExists"];
+					}
+					if (!fileDetailsJson["cloudPath"].is_null())
+					{
+						data.fileDetails.updatedAt = fileDetailsJson["cloudPath"];
+					}
+				}
+			}
+		}
+	};
+}
---
+++ RuyiSDKCpp/RuyiNet/Service/RuyiNetLobby.cpp
@@ -0,0 +1,74 @@
+#include "RuyiNetLobby.h"
+
+namespace Ruyi 
+{
+	RuyiNetLobby::RuyiNetLobby() 
+	{
+		PendingMembers.clear();
+		Members.clear();
+		MemberProfileIds.clear();
+
+		LobbyId = "";
+		OwnerProfileId = "";
+		State = "";
+		CreatedAt = 0;
+		UpdatedAt = 0;
+		RequestingPendingMemberCount = 0;
+		InvitedPendingMemberCount = 0;
+		MemberCount = 0;
+
+		MaxSlots = 0;
+		FreeSlots = 0;
+		ConnectionString = "";
+		LobbyType = RuyiNetLobbyType::PLAYER;
+	}
+
+	RuyiNetLobby::RuyiNetLobby(RuyiNetResponseGroup& response)
+	{
+		InitWithResponse(response);
+	}
+
+	RuyiNetLobby::~RuyiNetLobby()
+	{
+		PendingMembers.clear();
+		Members.clear();
+		MemberProfileIds.clear();
+	}
+
+	void RuyiNetLobby::InitWithResponse(RuyiNetResponseGroup& response)
+	{
+		ParseMembers(response.pendingMembers, PendingMembers);
+		ParseMembers(response.members, Members);
+
+		MemberProfileIds.clear();
+		std::for_each(Members.begin(), Members.end(), [&](RuyiNetLobbyMember& member)
+		{
+			MemberProfileIds.push_back(&(member.GetProfileId()));
+		});
+
+		LobbyId = response.groupId;
+		OwnerProfileId = response.ownerId;
+		State = response.name;
+		CreatedAt = response.createdAt;
+		UpdatedAt = response.updatedAt;
+		RequestingPendingMemberCount = response.requestingPendingMemberCount;
+		InvitedPendingMemberCount = response.invitedPendingMemberCount;
+		MemberCount = response.memberCount;
+
+		MaxSlots = response.data.maxSlots;
+		FreeSlots = response.data.freeSlots;
+		ConnectionString = response.data.connectionString;
+		LobbyType = response.data.ranked ? RuyiNetLobbyType::RANKED : RuyiNetLobbyType::PLAYER;
+	}
+
+	void RuyiNetLobby::ParseMembers(std::map<std::string, RuyiNetResponseGroup::Member>& members, std::list<RuyiNetLobbyMember>& pendingMembers)
+	{
+		pendingMembers.clear();
+		std::for_each(members.begin(), members.end(), [&](const std::pair<std::string, RuyiNetResponseGroup::Member>& member) 
+		{
+			RuyiNetLobbyMember lobbyMember(member.first, member.second.role);
+			pendingMembers.push_back(lobbyMember);
+		});
+	}
+
+}
