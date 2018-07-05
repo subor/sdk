9b0a8757eb2defb2d7966627401b9e366fbe6bb7
Jake W <jake@playruyi.com>
2018-07-05 12:10:11 +0800

    Merge branch 'development' of http://bitbucket:7990/scm/ruyi/jade into feature/mobile_app
    # Conflicts:
    #	jade/commons/Common/GlobalVariable.cs
    #	jade/layer0/Layer0/Layer0.cs

---
+++ RuyiSDK/RuyiNet/Service/Friend/Response/RuyiNetFriendResponseSummaryData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ruyi.SDK.Online
+{
+    /// <summary>
+    /// Represents summary data from a response.
+    /// </summary>
+    [Serializable]
+    public class RuyiNetFriendResponseSummaryData
+    {
+        /// <summary>
+        /// The name of the player.
+        /// </summary>
+        public string profileName;
+
+        /// <summary>
+        /// The ID of the player.
+        /// </summary>
+        public string profileId;
+
+        /// <summary>
+        /// The resource location of the profile picture.
+        /// </summary>
+        public string pictureUrl;
+
+        /// <summary>
+        /// The summary data.
+        /// </summary>
+        public Dictionary<string, object> summaryFriendData;
+
+        /// <summary>
+        /// Email address.
+        /// </summary>
+        public string email;
+    }
+}
--- ServiceGenerated/Generated/RuYi/SDK/ExternalErrors/ExternalErrorsSDKDataTypes.Constants.cs
+++ ServiceGenerated/Generated/RuYi/SDK/ExternalErrors/ExternalErrorsSDKDataTypes.Constants.cs
@@ -22,8 +22,8 @@ namespace Ruyi.SDK.ExternalErrors
     static ExternalErrorsSDKDataTypesConstants()
     {
       ExternalErrorInfos tmp0 = new ExternalErrorInfos();
-      tmp0.Description = "";
       tmp0.ErrorCode = ExternalErrorCode.Err_0;
+      tmp0.Description = "";
       EXTERNALERRORLIST.Add(tmp0);
       ExternalErrorInfos tmp1 = new ExternalErrorInfos();
       tmp1.Description = "setting {0} is not found!";
@@ -34,16 +34,16 @@ namespace Ruyi.SDK.ExternalErrors
       tmp2.ErrorCode = ExternalErrorCode.Err_2;
       EXTERNALERRORLIST.Add(tmp2);
       ExternalErrorInfos tmp3 = new ExternalErrorInfos();
-      tmp3.ErrorCode = ExternalErrorCode.Err_3;
       tmp3.Description = "json string convert failed: {0}";
+      tmp3.ErrorCode = ExternalErrorCode.Err_3;
       EXTERNALERRORLIST.Add(tmp3);
       ExternalErrorInfos tmp4 = new ExternalErrorInfos();
       tmp4.ErrorCode = ExternalErrorCode.Err_4;
       tmp4.Description = "lambda expression error: {0}";
       EXTERNALERRORLIST.Add(tmp4);
       ExternalErrorInfos tmp5 = new ExternalErrorInfos();
-      tmp5.ErrorCode = ExternalErrorCode.Err_5;
       tmp5.Description = "[type error] trying to assign a {0} to setting category {1} 's property {2}, whose data type is {3}";
+      tmp5.ErrorCode = ExternalErrorCode.Err_5;
       EXTERNALERRORLIST.Add(tmp5);
       ExternalErrorInfos tmp6 = new ExternalErrorInfos();
       tmp6.ErrorCode = ExternalErrorCode.Err_6;
@@ -58,28 +58,28 @@ namespace Ruyi.SDK.ExternalErrors
       tmp8.ErrorCode = ExternalErrorCode.Err_8;
       EXTERNALERRORLIST.Add(tmp8);
       ExternalErrorInfos tmp9 = new ExternalErrorInfos();
-      tmp9.Description = "user is not found by id {0}";
       tmp9.ErrorCode = ExternalErrorCode.Err_9;
+      tmp9.Description = "user is not found by id {0}";
       EXTERNALERRORLIST.Add(tmp9);
       ExternalErrorInfos tmp10 = new ExternalErrorInfos();
-      tmp10.ErrorCode = ExternalErrorCode.Err_10;
       tmp10.Description = "";
+      tmp10.ErrorCode = ExternalErrorCode.Err_10;
       EXTERNALERRORLIST.Add(tmp10);
       ExternalErrorInfos tmp11 = new ExternalErrorInfos();
-      tmp11.Description = "";
       tmp11.ErrorCode = ExternalErrorCode.Err_11;
+      tmp11.Description = "";
       EXTERNALERRORLIST.Add(tmp11);
       ExternalErrorInfos tmp12 = new ExternalErrorInfos();
       tmp12.ErrorCode = ExternalErrorCode.Err_12;
       tmp12.Description = "";
       EXTERNALERRORLIST.Add(tmp12);
       ExternalErrorInfos tmp13 = new ExternalErrorInfos();
-      tmp13.Description = "";
       tmp13.ErrorCode = ExternalErrorCode.Err_13;
+      tmp13.Description = "";
       EXTERNALERRORLIST.Add(tmp13);
       ExternalErrorInfos tmp14 = new ExternalErrorInfos();
-      tmp14.Description = "";
       tmp14.ErrorCode = ExternalErrorCode.Err_14;
+      tmp14.Description = "";
       EXTERNALERRORLIST.Add(tmp14);
       ExternalErrorInfos tmp15 = new ExternalErrorInfos();
       tmp15.Description = "";
@@ -94,8 +94,8 @@ namespace Ruyi.SDK.ExternalErrors
       tmp17.ErrorCode = ExternalErrorCode.Err_17;
       EXTERNALERRORLIST.Add(tmp17);
       ExternalErrorInfos tmp18 = new ExternalErrorInfos();
-      tmp18.Description = "trigger action failed when changing setting {0}!";
       tmp18.ErrorCode = ExternalErrorCode.Err_18;
+      tmp18.Description = "trigger action failed when changing setting {0}!";
       EXTERNALERRORLIST.Add(tmp18);
     }
   }
