fe250d0dff28366880718405152c203b01989a69
Jake W <jake@playruyi.com>
2018-07-27 17:14:21 +0800

    [Layer0] Comments for MemoryPool

--- SDK.BaseAsync/MemoryPool.cs
+++ SDK.BaseAsync/MemoryPool.cs
@@ -3,18 +3,27 @@
 namespace Ruyi.Common
 {
     /// <summary>
-    /// 
+    /// Pooled <see cref="System.IO.MemoryStream"/>
     /// </summary>
     /// <remarks>
-    /// http://www.philosophicalgeek.com/2015/02/06/announcing-microsoft-io-recycablememorystream/
+    /// Via http://www.philosophicalgeek.com/2015/02/06/announcing-microsoft-io-recycablememorystream/
     /// </remarks>
     public static class MemoryPool
     {
+        /// <summary>
+        /// Gets a <see cref="MemoryStream"/> from the pool.
+        /// </summary>
+        /// <returns></returns>
         public static MemoryStream GetStream()
         {
             return streamManager.GetStream();
         }
 
+        /// <summary>
+        /// Gets a <see cref="MemoryStream"/> from the pool initialized with <paramref name="buffer"/>.
+        /// </summary>
+        /// <param name="buffer">The buffer.</param>
+        /// <returns></returns>
         public static MemoryStream GetStream(byte[] buffer)
         {
             return streamManager.GetStream(string.Empty, buffer, 0, buffer.Length);
