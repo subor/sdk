593e3c5eaa46dd84012f5c399e6fb62679504b21
Jake W <jake@playruyi.com>
2018-08-03 13:36:46 +0800

    [Build] Fix assembly metadata for packaging and publish to nuget (JP-1434)

--- RuyiLogger.nf2.0/Properties/AssemblyInfo.cs
+++ RuyiLogger.nf2.0/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RuyiLogger.nf2.0")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Subor.SDK.Net35")]
+[assembly: AssemblyDescription("Subor SDK targetting .NET Framework 3.5 (for Unity3D)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RuyiLogger.nf2.0")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Subor Advanced Technology Co., Ltd.")]
+[assembly: AssemblyProduct("Subor.SDK")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0c3f3187-05ab-4579-a87a-7798dd22b09c")]
+[assembly: Guid("690773dd-f715-4956-9471-357b1f81d933")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
