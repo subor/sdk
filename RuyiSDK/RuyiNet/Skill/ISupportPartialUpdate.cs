99f6fbc0b692990eefb1d208783f2e3c6419c003
PLAYRUYIlei <jinlei@playruyi.com>
2018-08-24 19:00:48 +0800

    [Layer0] put the debug flag on PublishEndPoint, so we don't need to create an specific PublishServer for debugger; lock the PublishEndPoint if they may get invoked from mutiple thread.

---
+++ RuyiSDK/RuyiNet/Skill/ISupportPartialUpdate.cs
@@ -0,0 +1,13 @@
+﻿namespace Ruyi.SDK.Online
+{
+    /// <summary>
+    /// An interface for players that support partial updates.
+    /// </summary>
+    public interface ISupportPartialUpdate
+    {
+        /// <summary>
+        /// The partial update percentage.
+        /// </summary>
+        double PartialUpdatePercentage { get; }
+    }
+}
---
+++ SDK.BaseAsync/SDKUtility.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Ruyi
+{
+    public class SDKUtility
+    {
+        public static SDKUtility Instance { get; } = new SDKUtility();
+
+        public int LowLatencyTimeout => Debugger.IsAttached ? 6000000 : 20000;
+
+        public int HighLatencyTimeout => Debugger.IsAttached ? 6000000 : 40000;
+    }
+}
