9b3ccae78fc6f09430f4572da0aa55014a54c9c0
PLAYRUYIray <ray.tang@playruyi.com>
2018-09-06 14:48:06 +0800

    [JP-2599] implement virtual keyboard "input password" mode

---
+++ RuyiSDKCpp/RuyiNet/Service/Party/RuyiNetPartyService.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "../../Response/RuyiNetGetPartyInfoResponse.h"
+#include "../../Response/RuyiNetGetProfilesResponse.h"
+#include "../../Response/RuyiNetResponse.h"
+#include "../RuyiNetService.h"
+
+namespace Ruyi { namespace SDK { namespace Online {
+
+	class RuyiNetPartyService : public RuyiNetService
+	{
+	public:
+		RuyiNetPartyService(RuyiNetClient * client);
+
+		/// <summary>
+		/// Get the current party information for the player.
+		/// </summary>
+		/// <param name="index">The index of user</param>
+		/// <param name="response">The parse data structure of response json</param>
+		void GetPartyInfo(int index, RuyiNetGetPartyInfoResponse& response);
+
+		/// <summary>
+		/// Get information on the party members in the current party.
+		/// </summary>
+		/// <param name="index">The index of user</param>
+		/// <param name="response">The parse data structure of response json</param>
+		void GetPartyMembersInfo(int index, RuyiNetGetProfilesResponse& response);
+		
+		/// <summary>
+		/// Invite someone to join a party.
+		/// </summary>
+		/// <param name="index">The index of user</param>
+		/// <param name="profileId">The profile ID of the player to invite.</param>
+		/// <param name="response">The parse data structure of response json</param>
+		void SendPartyInvitation(int index, const std::string& profileId, RuyiNetResponse& response);		
+		
+		/// <summary>
+		/// Accept a party invitation.
+		/// </summary>
+		/// <param name="index">The index of user</param>
+		/// <param name="groupId">The group ID of the party to join.</param>
+		/// <param name="response">The parse data structure of response json</param>
+		void AcceptPartyInvitation(int index, const std::string& groupId, RuyiNetResponse& response);
+		
+		/// <summary>
+		/// Reject a party invitation.
+		/// </summary>
+		/// <param name="index">The index of user</param>
+		/// <param name="groupId">The group ID of the party to reject.</param>
+		/// <param name="response">The parse data structure of response json</param>
+		void RejectPartyInvitation(int index, const std::string& groupId, RuyiNetResponse& response);
+		
+		/// <summary>
+		/// Join a friend's party.
+		/// </summary>
+		/// <param name="index">The index of user</param>
+		/// <param name="groupId">The group ID of the party to join.</param>
+		/// <param name="response">The parse data structure of response json</param>
+		void JoinParty(int index, const std::string& groupId, RuyiNetResponse& response);
+		
+		/// <summary>
+		/// Leave a party.
+		/// </summary>
+		/// <param name="index">The index of user</param>
+		/// <param name="groupId">The group ID of the party to leave.</param>
+		/// <param name="response">The parse data structure of response json</param>
+		void LeaveParty(int index, const std::string& groupId, RuyiNetResponse& response);	
+	};
+
+}}} 
---
+++ docs/topics/console_dev.md
@@ -0,0 +1,9 @@
+# Console Development
+
+## Tips
+
+* Stop UAC prompt from disconnecting Remote Desktop
+    1. __Start / Edit group policy__
+    1. Computer Configuration / Windows Settings / Security Settings / Local Policies / Security Options
+    1. Double-click __User Account Control: Switch to the secure desktop when prompting for elevation__
+    1. Click __Disabled__ and __OK__
