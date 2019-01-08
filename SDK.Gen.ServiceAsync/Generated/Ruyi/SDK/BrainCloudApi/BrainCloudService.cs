cef735e13473a6bea0ca373fad274859a57e22b4
jake <jake@playruyi.com>
2019-01-08 11:32:44 +0800

    [SDK] Regenerate braincloud

--- SDK.Gen.ServiceAsync/Generated/Ruyi/SDK/BrainCloudApi/BrainCloudService.cs
+++ SDK.Gen.ServiceAsync/Generated/Ruyi/SDK/BrainCloudApi/BrainCloudService.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2ced9badde8f005c5b9c166658ab89543086964a51cfe86796a28313bd68ef6
+oid sha256:bd1d19b6e371afb73a08f4380797be3938ca9b7e15bc4dc6bc1bdf61eb6cee27
 size 4834577
